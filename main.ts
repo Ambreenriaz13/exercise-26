38a7c23f95e701d5d89e0fb1123b7cb397e5e3c3
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-05-24 22:33:42 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,21 @@
+// Define varibale
+let alienColor ="green"
+
+// first version
+if (alienColor =="green"){
+    console.log("player just earned 5 points for shooting the alien")
+}
+
+else{
+    console.log("player just earned 10 points ");
+}
+
+//second version
+
+ if (alienColor == "blue"){
+
+    console.log("I am come from if Statement")
+ }
+  else{
+    console.log("I am come from Else Statement");
+  }
